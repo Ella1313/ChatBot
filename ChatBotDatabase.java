c3668e8395b18f583e0a8082ce39dc62415e4f40
Ella1313 <60152348+Ella1313@users.noreply.github.com>
2023-07-01 12:39:57 -0700

    Update ChatBotDatabase.java

--- ChatBotDatabase.java
+++ ChatBotDatabase.java
@@ -5,15 +5,19 @@ public class ChatBotDatabase {
     public static void main(String[] args) {
 
          String url = "jdbc:sqlite:chatbot.db";
-
+        
+        //Database Connection)
         try (Connection con = DriverManager.getConnection(url)) {
-
+            
+            // Create Users table
             String createUsersTable = "Create Table users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)";
             executeStatement(con, createUsersTable);
-
+            
+            //Create command Table
             String createCommandsTable = "Create table commands (id INTEGER, command TEXT)";
             executeStatement(con, createCommandsTable);
 
+            //Create Chat History Table
             String createChatHistoryTable = "Create table chatHistory (id INTEGER PRIMARY KEY,command_id INTEGER REFERENCES Commands(id), timestamp TEXT)";
             executeStatement(con, createChatHistoryTable);
 
@@ -22,6 +26,7 @@ public class ChatBotDatabase {
         }
 
         }
+    
     private static void executeStatement(Connection con, String createUsersTable) {
         try (Statement statement = con.createStatement()) {
             statement.execute(createUsersTable);
@@ -29,6 +34,8 @@ public class ChatBotDatabase {
             e.printStackTrace();
         }
     }
+
+       // To verify the tables exist in Database
  /*   private static boolean checkTableExists(Connection connection, String tableName) throws SQLException {
         DatabaseMetaData metadata = connection.getMetaData();
         ResultSet resultSet = metadata.getTables(null, null, tableName, null);
